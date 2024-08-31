32d0f7dd9b6ec921686057a6123581fb6696fb9f
Calvin Lawrence <Calvin Lawrence@MSI>
2024-08-31 23:53:31 +0800

    ReactIconUpdate1

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -11,16 +11,16 @@ const Tools = () => {
   return (
     <section id="tools">
         <h2 className="my-10 text-center text-3xl lg:text-8xl text-white font-bold">Tools</h2>
-        <div className="flex items-center justify-center space-x-4">
-        <div className="flex space-x-4">
+        <div className="flex items-center justify-center space-x-4 flex-wrap">
+        <div className="flex flex-wrap space-x-4 justify-center">
                 {/* <img  alt="Figma" className="hover:scale-110 transition-transform duration-300 w-10 h-10" /> */}
-                <SiAdobephotoshop className="hover:scale-110 transition-transform duration-300 w-20 h-20 text-[#001E36]" />
-                <FaFigma className="hover:scale-110 transition-transform duration-300 w-20 h-20 text-[#F24E1E]" />
-                <RiTailwindCssFill className="hover:scale-110 transition-transform duration-300 w-20 h-20 text-[#38BDF8]"/>
-                <FaReact className="hover:scale-110 transition-transform duration-300 w-20 h-20 text-[#58C4DC]" />
-                <FaPhp className="hover:scale-110 transition-transform duration-300 w-20 h-20 text-[#8892BF]" />
-                <SiMysql className="text-[#F29111] hover:scale-110 transition-transform duration-300 w-20 h-20" />
-                <IoLogoJavascript className="text-[#F7DF1E] hover:scale-110 transition-transform duration-300 w-20 h-20" />
+                <SiAdobephotoshop className="hover:scale-110 transition-transform duration-300 w-20 h-20 text-[#001E36] sm:w-16 sm:h-16" />
+                <FaFigma className="hover:scale-110 transition-transform duration-300 w-20 h-20 text-[#F24E1E] sm:w-16 sm:h-16" />
+                <RiTailwindCssFill className="hover:scale-110 transition-transform duration-300 w-20 h-20 text-[#38BDF8] sm:w-16 sm:h-16"/>
+                <FaReact className="hover:scale-110 transition-transform duration-300 w-20 h-20 text-[#58C4DC] sm:w-16 sm:h-16" />
+                <FaPhp className="hover:scale-110 transition-transform duration-300 w-20 h-20 text-[#8892BF] sm:w-16 sm:h-16" />
+                <SiMysql className="text-[#F29111] hover:scale-110 transition-transform duration-300 w-20 h-20 sm:w-16 sm:h-16" />
+                <IoLogoJavascript className="text-[#F7DF1E] hover:scale-110 transition-transform duration-300 w-20 h-20 sm:w-16 sm:h-16" />
             </div>
         </div>
     </section>
